bd7241cce4ae7652360b91905657663637813713
unknown <andierose.umilda@gmail.com>
2018-05-28 19:43:30 +0800

    add before and after

--- src/test/java/stepDefinitions/calendarStepDef.java
+++ src/test/java/stepDefinitions/calendarStepDef.java
@@ -1,18 +1,27 @@
 package stepDefinitions;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class calendarStepDef {
+
+    public static WebDriver driver;
+
     @Given("^i launch the chrome$")
     public void iLaunchTheChrome() {
-        System.out.println("Test only");
+        // open chrome browser
+        System.setProperty("webdriver.chrome.driver","..\\automation\\seleniumWebBrowser\\chromedriver.exe");
+        driver = new ChromeDriver();
     }
 
     @When("^i open the IBE page$")
     public void iOpenTheIBEPage() {
+        // open the IBE page
+        driver.get("http://www.google.com");
         System.out.println("Test only");
     }
 
