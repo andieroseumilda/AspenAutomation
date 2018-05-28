1d9cad1957b1877f006b98f3c2efcab41cf726e8
unknown <andierose.umilda@gmail.com>
2018-05-28 19:46:38 +0800

    update before and after

--- src/test/java/features/internetBookingEngineMobile.feature
+++ src/test/java/features/internetBookingEngineMobile.feature
@@ -2,6 +2,6 @@ Feature: I should be able to book a reservation by using smartphone
 
 
   Scenario: I should be able to select a stay dates
-    Given i launch the chrome
-    When i open the IBE page
+    Given i open the IBE page
+    When i should see the Google label
     Then i verify that the hotel property is on the page
--- src/test/java/stepDefinitions/calendarStepDef.java
+++ src/test/java/stepDefinitions/calendarStepDef.java
@@ -1,5 +1,8 @@
 package stepDefinitions;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -11,22 +14,34 @@ public class calendarStepDef {
 
     public static WebDriver driver;
 
-    @Given("^i launch the chrome$")
-    public void iLaunchTheChrome() {
-        // open chrome browser
+
+    @Before
+    public void initialize(){
         System.setProperty("webdriver.chrome.driver","..\\automation\\seleniumWebBrowser\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 
-    @When("^i open the IBE page$")
+    @Given("^i open the IBE page$")
     public void iOpenTheIBEPage() {
         // open the IBE page
         driver.get("http://www.google.com");
         System.out.println("Test only");
     }
 
-    @Then("^i verify that the hotel property is on the page$")
-    public void iVerifyThatTheHotelPropertyIsOnThePage() {
-        System.out.println("Test only");
+    @When("^i should see the Google label$")
+    public void iShouldSeeTheGoogleLabel() {
+        System.out.println("Google copy");
     }
+
+    @Then("^i verify that the hotel property is on the page$")
+    public void iVerifyThatTheHotelPropertyIsOnThePage() throws InterruptedException {
+        System.out.println("Test only");
+        Thread.sleep(3000);
+    }
+
+    @After
+    public void closeTheWindow(){
+        driver.quit();
+    }
+
 }
